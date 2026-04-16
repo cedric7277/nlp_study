95909bcec6c9467da7991b0839c4ba2fb37bd00b
zhangfulv <282734967@qq.com>
2026-04-16 11:09:47 +0800

    提交第一周作业,展示配置环境

--- 张福/week01/week01.py
+++ 张福/week01/week01.py
@@ -7,4 +7,19 @@ if __name__ == '__main__':
         注意3⚠️：注意【不要修改其他同学和老师提交的文件】
         注意4⚠️：注意【文件名中不要包含 `空格`  或 `.` 等特殊符号( 可用 `"-"` 、`"_"` )】
         注意5⚠️：提交作业后只需等待，老师会安排时间审批通过，通过后会在老师仓库中显示。
+        
+        # Name                     Version          Build            Channel
+        gensim                     4.4.0            pypi_0           pypi
+        matplotlib                 3.9.2            pypi_0           pypi
+        matplotlib-inline          0.2.1            pypi_0           pypi
+        matplotlib-venn            1.1.2            pypi_0           pypi
+        numpy                      1.26.4           pypi_0           pypi
+        pandas                     2.2.2            pypi_0           pypi
+        peft                       0.15.0           pypi_0           pypi
+        scikit-learn               1.5.1            pypi_0           pypi
+        torch                      2.11.0           pypi_0           pypi
+        torchvision                0.26.0           pypi_0           pypi
+        transformers               4.55.0           pypi_0           pypi
+        ------------
+        Python 3.12.0
     """)
